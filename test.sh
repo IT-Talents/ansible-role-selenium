2bc3b8a775b7025193d7605db6243b9302db2d3c
MassiveHiggsField <benjamin.bittner@omigos.de>
2016-11-07 11:38:32 +0100

    fixed centos 6, added new tests

--- test.sh
+++ test.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
-distro="geerlingguy/docker-centos7-ansible:latest"
-init="/usr/lib/systemd/systemd"
+set -e
+
+distro="geerlingguy/docker-ubuntu1604-ansible:latest"
+init="/lib/systemd/systemd"
 run_opts="--privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
 container_id=$(mktemp)
 idempotence=$(mktemp)
 
+docker rm -f $(sudo docker ps -a -q)
 docker pull ${distro}
+
 docker run --detach --volume="${PWD}":/etc/ansible/roles/role_under_test:ro ${run_opts} ${distro} "${init}" > "${container_id}"
+
 docker exec --tty "$(cat ${container_id})" env TERM=xterm ansible-galaxy install -r /etc/ansible/roles/role_under_test/tests/requirements.yml
-#docker exec --tty "$(cat ${container_id})" env TERM=xterm ansible-playbook /etc/ansible/roles/role_under_test/tests/test.yml --syntax-check
+
+docker exec --tty "$(cat ${container_id})" env TERM=xterm ansible-playbook /etc/ansible/roles/role_under_test/tests/test.yml --syntax-check
+
+docker exec --tty "$(cat ${container_id})" env TERM=xterm ansible-playbook /etc/ansible/roles/role_under_test/tests/test.yml
 docker exec --tty "$(cat ${container_id})" env TERM=xterm ansible-playbook /etc/ansible/roles/role_under_test/tests/test.yml
 
 cat ${container_id}
-
-#sudo docker exec "$(cat ${container_id})" ansible-playbook /etc/ansible/roles/role_under_test/tests/test.yml | tee -a ${idempotence}
-#tail ${idempotence} grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
