cb72a3196573809e9f7e88881483e21606009432
MassiveHiggsField <benjamin.bittner@omigos.de>
2016-11-07 20:14:35 +0100

    reactivated wait and deactivated all distros but ubuntu 16.04 for travis stability tests

--- test.sh
+++ test.sh
@@ -8,16 +8,27 @@ run_opts="--privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
 container_id=$(mktemp)
 idempotence=$(mktemp)
 
+echo "removing all docker containers..."
 docker rm -f $(sudo docker ps -a -q)
+
+echo "pulling distro..."
 docker pull ${distro}
 
+echo "creating docker image..."
 docker run --detach --volume="${PWD}":/etc/ansible/roles/role_under_test:ro ${run_opts} ${distro} "${init}" > "${container_id}"
 
+echo "using container id: $(cat ${container_id})"
+
+echo "install galaxy deps..."
 docker exec --tty "$(cat ${container_id})" env TERM=xterm ansible-galaxy install -r /etc/ansible/roles/role_under_test/tests/requirements.yml
 
+echo "checking syntax..."
 docker exec --tty "$(cat ${container_id})" env TERM=xterm ansible-playbook /etc/ansible/roles/role_under_test/tests/test.yml --syntax-check
 
-docker exec --tty "$(cat ${container_id})" env TERM=xterm ansible-playbook /etc/ansible/roles/role_under_test/tests/test.yml
+echo "first test run..."
 docker exec --tty "$(cat ${container_id})" env TERM=xterm ansible-playbook /etc/ansible/roles/role_under_test/tests/test.yml
 
-cat ${container_id}
+echo "second test run for idempotency..."
+docker exec --tty "$(cat ${container_id})" env TERM=xterm ansible-playbook /etc/ansible/roles/role_under_test/tests/test.yml
+
+echo "used container id: $(cat ${container_id})"
