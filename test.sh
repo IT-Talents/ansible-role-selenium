16a84bde0fc085d2fa363b1c974e1fa893b131d3
MassiveHiggsField <benjamin.bittner@omigos.de>
2016-11-09 23:22:33 +0100

    refactored test script and integrated it into travis build

--- test.sh
+++ test.sh
@@ -1,36 +1,120 @@
 #!/usr/bin/env bash
 
-distro="geerlingguy/docker-centos6-ansible:latest"
-init="/sbin/init"
-run_opts="--privileged"
+set -e
+
+PULL=0
+IDEM=0
+REMOVE=0
+SYNTAX=0
+VERBOSE=
+
+while [[ $# -gt 1 ]]
+    do
+    key="$1"
+
+    case $key in
+        -d|--distro)
+        DISTRO="$2"
+        shift
+        ;;
+        -p|--pull)
+        PULL=1
+        ;;
+        -i|--idempotency)
+        IDEM=1
+        ;;
+        -r|--remove)
+        REMOVE=1
+        ;;
+        -s|--syntax)
+        SYNTAX=1
+        ;;
+        -v|--verbose)
+        VERBOSE="-vvv"
+        ;;
+        *)
+            # unknown option
+        ;;
+    esac
+    shift
+done
+
+if [ "${DISTRO}" == "" ] ; then
+    echo "usage: ./start.sh [OPTIONS]... -d|--distro [DISTRO] "
+    echo "   -d|-distro        sets the distro to be used (ubuntu1604, ubuntu1404, ubuntu1204, centos7, centos6, debian8)"
+    echo "   -p|-pull          distro will be pulled from repo"
+    echo "   -i|-idempotency   idempotency check will be run"
+    echo "   -r|-remove        removes ALL current docker containers before running any checks"
+    echo "   -s|-syntax        runs syntax check"
+    exit 1;
+fi
+
+declare -A DISTROS
+DISTROS[ubuntu1604]="geerlingguy/docker-ubuntu1604-ansible:latest"
+DISTROS[ubuntu1404]="geerlingguy/docker-ubuntu1404-ansible:latest"
+DISTROS[ubuntu1204]="geerlingguy/docker-ubuntu1204-ansible:latest"
+DISTROS[centos6]="geerlingguy/docker-centos6-ansible:latest"
+DISTROS[centos7]="geerlingguy/docker-centos7-ansible:latest"
+DISTROS[debian8]="geerlingguy/docker-debian8-ansible:latest"
+
+declare -A INITS
+INITS[ubuntu1604]="/lib/systemd/systemd"
+INITS[ubuntu1404]="/sbin/init"
+INITS[ubuntu1204]="/sbin/init"
+INITS[centos6]="/sbin/init"
+INITS[centos7]="/usr/lib/systemd/systemd"
+INITS[debian8]="/lib/systemd/systemd"
+
+declare -A OPTS
+OPTS[ubuntu1604]="--privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
+OPTS[ubuntu1404]="--privileged"
+OPTS[ubuntu1204]="--privileged"
+OPTS[centos6]="--privileged"
+OPTS[centos7]="--privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
+OPTS[debian8]="--privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
+
 container_id=$(mktemp)
 idempotence=$(mktemp)
 
-echo "removing all docker containers..."
-docker rm -f $(sudo docker ps -a -q)
+if [ "${DISTROS[${DISTRO}]}" == "" ] ; then
+    echo "unkown distro, available: ubuntu1604, ubuntu1404, ubuntu1204, centos7, centos6, debian8"
+    exit 1;
+fi
 
-set -e
+if [ ${REMOVE} -eq 1 ] ; then
+    echo "removing all docker containers..."
+    docker rm -f $(sudo docker ps -a -q) || true
+fi
 
-echo "pulling distro..."
-docker pull ${distro}
-
-echo "switching to urandom"
-docker run -v /dev/urandom:/dev/random
+if [ ${PULL} -eq 1 ] ; then
+    echo "pulling distro..."
+    docker pull ${DISTROS[${DISTRO}]}
+fi
 
 echo "creating docker image..."
-docker run --detach --volume="${PWD}":/etc/ansible/roles/role_under_test:ro ${run_opts} ${distro} "${init}" > "${container_id}"
+docker run --detach -v /dev/urandom:/dev/random --volume="${PWD}":/etc/ansible/roles/role_under_test:ro ${OPTS[${DISTRO}]} ${DISTROS[${DISTRO}]} "${INITS[${DISTRO}]}" > "${container_id}"
 
 echo "using container id: $(cat ${container_id})"
 
 echo "install galaxy deps..."
 docker exec --tty "$(cat ${container_id})" env TERM=xterm ansible-galaxy install -r /etc/ansible/roles/role_under_test/tests/requirements.yml
 
-echo "checking syntax..."
-docker exec --tty "$(cat ${container_id})" env TERM=xterm ansible-playbook /etc/ansible/roles/role_under_test/tests/test.yml --syntax-check
+if [ ${SYNTAX} -eq 1 ] ; then
+    echo "checking syntax..."
+    docker exec --tty "$(cat ${container_id})" env TERM=xterm ansible-playbook /etc/ansible/roles/role_under_test/tests/test.yml --syntax-check
+fi
 
 echo "first test run..."
-docker exec --tty "$(cat ${container_id})" env TERM=xterm ansible-playbook /etc/ansible/roles/role_under_test/tests/test.yml
+docker exec --tty "$(cat ${container_id})" env TERM=xterm ansible-playbook ${VERBOSE} /etc/ansible/roles/role_under_test/tests/test.yml
 
+# Test role idempotence.
+#  - idempotence=$(mktemp)
+#  - sudo docker exec "$(cat ${container_id})" ansible-playbook /etc/ansible/roles/role_under_test/tests/test.yml | tee -a ${idempotence}
+#  - >
+#    tail ${idempotence}
+#    | grep -q 'changed=0.*failed=0'
+#    && (echo 'Idempotence test: pass' && exit 0)
+#    || (echo 'Idempotence test: fail' && exit 1)
 #echo "second test run for idempotency..."
 #docker exec --tty "$(cat ${container_id})" env TERM=xterm ansible-playbook /etc/ansible/roles/role_under_test/tests/test.yml
 
